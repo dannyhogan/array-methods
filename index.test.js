25e621f4502c0881e0d40074082008be8e6eea2d
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-25 09:49:10 -0700

    Created first test for filter function

--- index.test.js
+++ index.test.js
@@ -1,15 +1,20 @@
-const { map } = require('./index');
+const { map, filter } = require('./index');
 
-describe('test array method functions', () => {
-
+describe('map array function', () => {
   // Test for map function.
   it('squares array using map function', () => {
     const numsArray = [2, 4, 6];
     const squared = map(numsArray, num => num * num);
     expect(squared).toEqual([4, 16, 36]);
   });
+});
 
-  // Test for 
+describe('filter array function', () => {
+  it('returns an array', () => {
+    const numbers = [1, 2, 3];
+    const filtered = filter(numbers, num => {num * 2})
+    expect(filtered).toEqual(expect.any(Array));
+  });
 
-
+  
 });
