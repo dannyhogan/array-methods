129c95396353b21f9bbbbba65f9191c6c39c48f1
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-25 10:05:36 -0700

    Created two additional tests for map function

--- index.test.js
+++ index.test.js
@@ -2,6 +2,18 @@ const { map, filter } = require('./index');
 
 describe('map array function', () => {
   // Test for map function.
+  it('returns an array', () => {
+    const numbers = [1, 2, 3];
+    const mapped = map(numbers, num => num * 2)
+    expect(mapped).toEqual(expect.any(Array));
+  });
+
+  it('mapped array equals arr', () => {
+    const numbers = [1, 2, 3];
+    const mapped = map(numbers, num => num * 2);
+    expect(mapped).toHaveLength(numbers.length);
+  });
+
   it('squares array using map function', () => {
     const numsArray = [2, 4, 6];
     const squared = map(numsArray, num => num * num);
