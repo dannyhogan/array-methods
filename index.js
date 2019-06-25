98323d5ee3f54fc74fa2d7db95ef24e3cb02dcd8
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-25 09:49:00 -0700

    Created filter function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function map (array, cb) {
+function map(array, cb) {
     let newArray = [];
     for (let i=0; i < array.length; i++) {
       newArray[i] = cb(array[i]);
@@ -6,4 +6,20 @@ function map (array, cb) {
     return newArray;
 }
 
-module.exports = { map }
+//Define new filtered array and return it
+//Iterate over arr, set filted equal to it
+//Add conditional for callback
+
+function filter(array, cb) {
+  let filtered = [];
+  for(let i = 0; i < array.length; i++) {
+    if(cb(array[i])) {
+      filtered[i] = array[i];
+    }
+  }
+  return filtered;
+}
+
+module.exports = { map, filter }
+
+
